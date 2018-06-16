876f8d433901ee304e5c830c61e18ba1600d554e
cwocwo <cwocwo1@gmail.com>
2018-06-16 14:59:58 +0800

    export bookinfo images

--- bookinfo.sh
+++ bookinfo.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
-
-function create_dir_ifnotexist {
-  if [ ! -d "$1" ];then
-    mkdir $1
-  else
-    echo "$1 already exists."
-  fi
-}
-
-
-source ./env.sh
+source ./release-conf.sh
+#source ./env.sh
 echo "$VERSION"
-build_dir="./build"
-create_dir_ifnotexist $build_dir
-sample_dir="$build_dir/samples/"
+sample_dir="${BUILD_DIR}samples/"
 create_dir_ifnotexist $sample_dir
-cp $build_dir/istio-$VERSION/samples/bookinfo/kube/bookinfo.yaml $sample_dir
+cp ${BUILD_DIR}istio-${VERSION}/samples/bookinfo/kube/bookinfo.yaml $sample_dir
 
-image_file=${sample_dir}images
+image_file=${sample_dir}images.txt
 grep "image: " $sample_dir/bookinfo.yaml|awk '{print $2}'>${image_file}
+
+# save images
+image_dir=${sample_dir}images/
+create_dir_ifnotexist ${image_dir}
+
+# generate load image script
+load_images_script=${image_dir}load-image.sh
+cat "" > $load_images_script
+
 
 for image_name in $(<${image_file});
   do
@@ -26,11 +24,18 @@ for image_name in $(<${image_file});
     echo "processing image: $image_name ......."
     echo "--------------------------------------------------------------------------------"
     echo "pull image: $image_name ......"
-    docker pull $image_name
+    #docker pull $image_name
+
+    export_image_name1=${image_name//\//-}
+    export_image_name=${export_image_name1/\:/-}.tar.gz
+    if [ ! -f $export_image_name ]; then docker save -o $image_dir$export_image_name $image_name; fi
+    echo "Add script: docker load < $export_image_name"
+    echo "docker load < $export_image_name">>$load_images_script
+
     image_name_local="${REPO}samples/$image_name"
     echo "docker tag $image_name $image_name_local"
     docker tag $image_name $image_name_local
-    docker push $image_name_local
+    #docker push $image_name_local
     
     sed -i "s|image: ${image_name}|image: ${image_name_local}|g" $sample_dir/bookinfo.yaml
   done
